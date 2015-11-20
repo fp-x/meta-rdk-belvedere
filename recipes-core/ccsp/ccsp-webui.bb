f8854ff9b45acf1d404d7148601417d619278c0d
Justin Laue <justin@fp-x.com>
2015-11-20 14:17:07 -0700

    Initial commit of webui

---
+++ recipes-core/ccsp/ccsp-webui.bb
@@ -0,0 +1,84 @@
+SUMMARY = "CCSP WebUI component"
+HOMEPAGE = "http://github.com/ccsp-yocto/webui"
+
+LICENSE = "Apache-2.0"
+LIC_FILES_CHKSUM = "file://README;beginline=1;endline=18;md5=17835e4bef073e5070ff4a4fc79fe537"
+
+DEPENDS = "ccsp-common-library php php-native"
+
+SRC_URI = "\
+    git://bitbucket.org/ccsptwc/webui;protocol=https;branch=api-cleanup \
+    file://LICENSE \
+    file://cosalogs.service \
+    file://cosalogs.sh \
+    file://ert-start.sh \
+    file://dbus-start.sh \
+    "
+
+SRCREV = "${AUTOREV}"
+SRCREV_xb3 = "${AUTOREV}"
+SRCREV_FORMAT = "default_xb3"
+
+# PV = "${RDK_RELEASE}+git${SRCPV}"
+
+S = "${WORKDIR}/git/source/CcspPhpExtension/"
+
+inherit autotools systemd
+
+CFLAGS += " \
+    -I${STAGING_INCDIR}/dbus-1.0 \
+    -I${STAGING_LIBDIR}/dbus-1.0/include \
+    -I${STAGING_INCDIR}/ccsp \
+    -fPIC \
+    "
+
+# LDFLAGS += " -ldbus-1 "
+
+do_configure_prepend () {
+    which phpize
+	cp -fr ${WORKDIR}/LICENSE ${S}
+	(cd ${S} && phpize && aclocal && libtoolize --force && autoreconf)
+    # ${STAGING_BINDIR_CROSS}/
+}
+
+EXTRA_OECONF = "--enable-cosa CCSP_COMMON_LIB=${STAGING_LIBDIR}"
+
+do_configure () {
+	oe_runconf
+}
+
+do_install() {
+#	oe_runmake install DESTDIR=${D} INSTALL_ROOT=${D}
+
+    install -d ${D}${base_libdir}/rdk
+	install -d ${D}${systemd_unitdir}/system
+	install -m 0644 ${WORKDIR}/cosalogs.service ${D}${systemd_unitdir}/system
+	install -m 0644 ${WORKDIR}/cosalogs.sh ${D}${base_libdir}/rdk
+    install -d ${D}/fss/gw/etc
+    install -d ${D}/fss/gw/usr/www
+    install -d ${D}/fss/gw/usr/ccsp
+    cp -r ${WORKDIR}/git/source/Styles/xb3/code/* ${D}/fss/gw/usr/www/
+    install ${WORKDIR}/git/source/Styles/xb3/config/php.ini ${D}/fss/gw/etc/
+    install ${WORKDIR}/ert-start.sh ${D}/fss/gw/usr/ccsp/
+    install ${WORKDIR}/dbus-start.sh ${D}/fss/gw/usr/ccsp/
+    install ${S}/modules/*.so ${D}/fss/gw/usr/ccsp/
+#    install -d ${D}/usr/lib/extensions/no-debug-non-zts-20100525/
+#    install ${S}/modules/*.so ${D}/usr/lib/extensions/no-debug-non-zts-20100525/
+    install -d ${D}/var/
+    touch ${D}/var/ui_dev_debug
+    touch ${D}/var/ui_dev_mode
+    touch ${D}/var/cosa_php_debug
+    install -d ${D}/var/tmp/logs
+    touch ${D}/var/tmp/logs/test
+}
+
+SYSTEMD_SERVICE_${PN} = "cosalogs.service"
+
+FILES_${PN} += "/fss/* /fss/gw/* /fss/gw/usr/* /fss/gw/etc/* /fss/gw/usr/www/* /fss/gw/usr/ccsp/* /opt/www/* ${base_libdir}/rdk/* /var/* ${libdir}"
+# /var/* /var/tmp/logs/* 
+
+FILES_${PN} += " ${systemd_unitdir}/system/cosalogs.service "
+# FILES_${PN} += " ${base_libdir}/rdk/* "
+FILES_${PN}-dbg += "/fss/gw/usr/ccsp/.debug/* /usr/lib/extensions/no-debug-non-zts-20100525/.debug/*"
+
+
---
+++ recipes-core/ccsp/ccsp-webui/ert-start.sh
@@ -0,0 +1,132 @@
+
+
+export LD_LIBRARY_PATH=$PWD:.:$PWD/lib:/lib:/usr/lib:$LD_LIBRARY_PATH
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/var/run/dbus/system_bus_socket
+
+# Start coredump
+if [ -f "$PWD/core_compr" ]; then
+        if ! [ -e "/var/core" ]; then
+                mkdir -p /var/core/
+        fi
+        echo "|$PWD/core_compr /var/core %p %e" >/proc/sys/kernel/core_pattern
+        ulimit -c unlimited
+
+        ./core_report.sh &
+fi
+
+# Start Config Recover
+echo "Starting Config Recover Daemon ..."
+conf_rec &
+
+cp ccsp_msg.cfg /tmp
+
+Subsys="eRT."
+
+echo "Elected subsystem is $Subsys"
+
+    echo "./CcspCrSsp -subsys $Subsys"
+    ./CcspCrSsp -subsys $Subsys
+    echo "./PsmSsp -subsys $Subsys"
+    ./PsmSsp -subsys $Subsys
+if [ -e ./pam ]; then
+    sleep 1
+	cd pam
+		echo "./CcspPandMSsp -subsys $Subsys"
+		./CcspPandMSsp -subsys $Subsys
+
+    # double background to detach the script from the tty
+	((sh ./email_notification_monitor.sh 12 &) &)
+	cd ..
+fi
+
+#sleep 3
+if [ -e ./rm ]; then
+cd rm
+    echo "./CcspRmSsp -subsys $Subsys"
+	./CcspRmSsp -subsys $Subsys
+cd ..
+fi
+
+
+if [ "x"$1 = "xpam" ] || [ "x"$2 = "xpam" ]; then
+  exit 0
+fi
+
+
+# Tr069Pa, as well as SecureSoftwareDownload and FirmwareUpgrade
+
+if [ -e ./wecb ]; then
+    sleep 5
+    cd wecb
+         echo "./CcspWecbController -subsys $Subsys"
+        ./CcspWecbController -subsys $Subsys 
+    cd ..
+fi
+
+if [ -e ./tr069pa ]; then
+    sleep 30
+    cd tr069pa
+        echo "./CcspTr069PaSsp -subsys $Subsys"
+        ./CcspTr069PaSsp -subsys $Subsys
+    cd ..
+fi
+
+if [ -e ./tr069pa ]; then
+    # add firewall rule to allow incoming packet for port 7547
+    sysevent setunique GeneralPurposeFirewallRule " -A INPUT -i erouter0 -p tcp --dport=7547 -j ACCEPT "
+fi
+
+if [ -e ./cm ]; then
+    sleep 5
+    cd cm
+        echo "./CcspCMAgentSsp -subsys $Subsys"
+        ./CcspCMAgentSsp -subsys $Subsys 
+    cd ..
+fi
+
+if [ -e ./mta ]; then
+    sleep 30
+    cd mta
+        echo "./CcspMtaAgentSsp -subsys $Subsys"
+        ./CcspMtaAgentSsp -subsys $Subsys 
+    cd ..
+fi
+
+if [ -e ./ssd ]; then
+    cd ssd
+    sleep 1
+        echo "./CcspSsdSsp -subsys $Subsys"
+        ./CcspSsdSsp -subsys $Subsys
+    cd ..
+fi
+
+if [ -e ./fu ]; then
+    cd fu
+    sleep 1
+        echo "./CcspFuSsp -subsys $Subsys"
+        ./CcspFuSsp -subsys $Subsys
+    cd ..
+fi
+
+if [ -e ./tad ]; then
+	cd tad
+	#delay TaD in order to reduce CPU overload and make PAM ready early
+	sleep 3
+        echo "./CcspTandDSsp -subsys $Subsys"
+		./CcspTandDSsp -subsys $Subsys
+	cd ..
+fi
+
+sleep 1
+
+if [ -e ./ccspRecoveryManager ]; then
+        echo "./ccspRecoveryManager -subsys $Subsys &"
+        ./ccspRecoveryManager -subsys $Subsys &
+fi
+
+
+if [ -e ./lm ]; then
+    cd lm
+    sleep 5
+    ./CcspLMLite &
+fi
